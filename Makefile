a26820da05f6fe7d9f5f06c93cd43b056e1136f6
edywardy1 <edywardy1@gmail.com>
2022-01-23 01:04:40 -0500

    fixing some overflow

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 audio: audio.o
-	g++ -o audio audio.o -std=c++1z -Wconversion -Wall -Werror -Wextra -pedantic
+	g++ -o audio audio.o -std=c++1z -Wconversion -Wall -Werror -Wextra -pedantic  -lfftw3 -lm
 audio.o: audio.cpp
-	g++ -c audio.cpp -std=c++1z -Wconversion -Wall -Werror -Wextra -pedantic
+	g++ -c audio.cpp -std=c++1z -Wconversion -Wall -Werror -Wextra -pedantic -lfftw3 -lm
--- audio.cpp
+++ audio.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
 #include "AudioFile.h"
 #include <fftw3.h>
+#include <complex.h>
+#include <vector>
+#include <cmath>
 
 using namespace std;
+
+//removes band of frequencies from dft array
+//input is freq in hertz
+void removeBand(vector<fftw_complex> &data, uint64_t start, uint64_t stop, int samplingRate, int numSamples) {
+    uint64_t startHz = (uint64_t) start * numSamples/samplingRate;
+    uint64_t stopHz = (uint64_t) stop * numSamples/samplingRate;
+    if(stopHz >= (uint64_t)data.size()) {
+        stopHz = (uint64_t)data.size() - 1;
+    }
+    if(startHz < 1) {
+        startHz = 0;
+    }
+    cout << start << "\n";
+    cout << stop << "\n";
+    cout << samplingRate << "\n";
+    cout << numSamples << "\n";
+    cout << startHz << " " << stopHz << "\n";
+    for(uint64_t i = startHz; i < stopHz; i++) {
+        data[i][0] = 0;
+        data[i][1] = 0;
+    }
+}
 
 int main(int argc, char** argv) {
     if (argc < 3) {
@@ -16,6 +41,7 @@ int main(int argc, char** argv) {
     audioFile.printSummary();
     int numSamples = audioFile.getNumSamplesPerChannel();
     int numChannels = audioFile.getNumChannels();
+    int samplingRate = audioFile.getSampleRate();
 
     //turn audio to mono if not
     if(!audioFile.isMono()) {
@@ -34,16 +60,40 @@ int main(int argc, char** argv) {
         audioFile.setNumChannels(1);
     }
 
+    //dft output complex vector
+    vector<fftw_complex> out(numSamples/2 + 1);
+    //dft setup
+    fftw_plan p = fftw_plan_dft_r2c_1d(numSamples, audioFile.samples[0].data(), out.data(), FFTW_ESTIMATE);
+    //idft setup
+    fftw_plan q = fftw_plan_dft_c2r_1d(numSamples, out.data(), audioFile.samples[0].data(), FFTW_ESTIMATE);
+    //execute dft on audioFile input into out vector
+    fftw_execute(p);
+    fftw_destroy_plan(p);
+    
+    //do filtering here
+    //removeBand(out, 240, 241, samplingRate, numSamples); 
+    //removeBand(out,100,125,samplingRate,numSamples);
+    //removeBand(out,275,300,samplingRate,numSamples);
+    //removeBand(out,400,425,samplingRate,numSamples);
+    //removeBand(out,700,725,samplingRate,numSamples);
+    //filtering out freq beyond human hearing
+    //removeBand(out, 0, 21, samplingRate, numSamples);
+    removeBand(out, 20000, (numSamples/2 +1), samplingRate, numSamples);
+    samplingRate++;
+    
+    //do idft and some cleanup
+    fftw_execute(q);
+    fftw_destroy_plan(q);
+    fftw_cleanup();
+    //normalize after idft
     for(int i = 0; i < numSamples; i++) {
-        audioFile.samples[0][i] *= 8;
+        audioFile.samples[0][i] /= numSamples;
     }
 
-    //write audiofile
+    //write audiofile to output file
     cout << "\n\n\nOutput File Summary:\n";
     audioFile.printSummary();
     audioFile.save(argv[2]);
-
-
 
     return 0;
 }
